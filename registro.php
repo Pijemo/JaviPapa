bd47f7ceace229747a63df8314e02a7d9e7d89f8
Javier Felix Perez Paz <javifpp@gmail.com>
2022-02-04 12:54:54 +0100

    04/02 para clonar

--- registro.php
+++ registro.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro usuario</title>
     <link rel="stylesheet" href="bootstrap5/css/bootstrap.css">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/index/styles.css">
 </head>
 <body>    
     <header class="cabecera">
@@ -20,5 +20,20 @@
             <div class="enlace"><a href="registro.html">Registrarse</a></div>
         </nav>
     </header>
+    <main>
+        <form action="" method="POST">
+            <label>Email:</label><input type="email">
+            <label>Contraseña:</label><input type="password">
+            <label>Confirmar Contraseña:</label><input type="password">
+            <label>Nombre:</label><input type="text">
+            <label>Apellido(s):</label><input type="text">
+            <label>Fecha de Nacimiento:</label><input type="date">
+            <label>Sexo:</label>
+            <label>Hombre</label><input type="radio" name="sexo">
+            <label>Mujer</label><input type="radio" name="sexo">
+            <label>Otro:</label><input type="text" name="sexo">
+            <input type="submit" value="enviar">
+        </form>
+    </main>
 </body>
 </html>
