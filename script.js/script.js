18b0cba5a2e09674600ef97a7d538b99980e1ed6
RudyGD <gonzalezdiesterudy@gmail.com>
2022-02-05 22:57:24 +0100

    05/02 menu responsive, faltan detalles

--- script.js/script.js
+++ script.js/script.js
@@ -1,28 +1,56 @@
 const crearHeader = () => {
+
+    console.log(window.screen.width)
     let header = document.querySelector(".cabecera");
-    console.log(header);
-    let numElementos = 6;
+    const numElementos = 6;
+    let divImg = document.createElement("div");
+    divImg.setAttribute("id", "logo");
+    let img = document.createElement("img");
+    let btn_opciones = document.createElement("button");
     let misDiv = [];
     let misEnlaces = [];
-    let divImg = document.createElement("div");
-    divImg.setAttribute("id","logo");
-    let img = document.createElement("img");
-    img.setAttribute("src", "img/logo2.png");
-    img.setAttribute("alt", "Logo");
-    
-    divImg.append(img);
-    let nav = document.createElement("nav");
-    nav.appendChild(divImg);
 
+    divImg.append(img);
+    let nav = document.createElement("nav");    
+    nav.appendChild(divImg);
+    let comprobarOpciones = true;
     for (let elemento = 0; elemento < numElementos; elemento++) {
         misDiv.push(document.createElement("div"));
         misDiv[elemento].classList.add("enlace");
+        
         misEnlaces.push(document.createElement("a"));
+        if(window.screen.width < 1300 && elemento > 0){ 
+            misDiv[elemento].style.display = "none";
+        }
         switch (elemento) {
             case 0:
-                let txtIndex = document.createTextNode("Index");
-                misEnlaces[0].setAttribute("href", "index.html");
-                misEnlaces[0].append(txtIndex);
+                if(window.screen.width < 1300){                    
+                    misEnlaces[0].append(img);
+                    img.setAttribute("src", "img/logo1.png");
+                    img.setAttribute("alt", "Logo");
+                    img.setAttribute("width", "85px");
+                    img.setAttribute("heigth", "85px");                    
+                    misEnlaces[0].setAttribute("width", "100%");
+                    misEnlaces[0].style.height="100px";
+                    
+                    
+
+                    btn_opciones.setAttribute("id", "btn_opciones");
+                    btn_opciones.style.height = "50px";
+                    btn_opciones.style.width = "90px";
+                    btn_opciones.style.margin = "30px 0";
+                    btn_opciones.style.background = "#e7684d";
+                    let txtx_btn =  document.createTextNode(". . .");
+                    btn_opciones.append(txtx_btn);  
+                                        
+                }else{
+                    img.setAttribute("src", "img/logo2.png");
+                    img.setAttribute("alt", "Logo");
+                    let txtIndex = document.createTextNode("Inicio");
+                    misEnlaces[0].append(txtIndex);
+                }                
+                misEnlaces[0].setAttribute("href", "index.php");
+               
                 break;
             case 1:
                 let txtNosotros = document.createTextNode("Nosotros");
@@ -50,13 +78,54 @@ const crearHeader = () => {
                 misEnlaces[5].append(txtRegistro);
                 break;
         }
-    misDiv[elemento].append(misEnlaces[elemento]);
-    nav.append(misDiv[elemento]);
-    //console.log(nav);
+        if (window.screen.width < 1300 && elemento > 0) {
+                misDiv[elemento].style.background = "#e7684d";
+                misDiv[elemento].style.borderRadius = "10%";
+                misDiv[elemento].style.width = "100%";
+                misDiv[elemento].style.border = "solid black 1px";
+                misDiv[elemento].style.marging = "15px"                 
+        }
+        
+        misDiv[elemento].append(misEnlaces[elemento]);
+               
+        nav.append(misDiv[elemento]);
+        if(comprobarOpciones && window.screen.width < 1300){
+                    nav.append(btn_opciones);
+                    nav.style.background = '#1897b2';
+                    nav.style.borderRadius = '2%'; 
+                    nav.style.padding ="15px"
+                              
+                    comprobarOpciones = false;
+                } 
+                
+        //console.log(nav);
+        
     }
-    header.append(nav);
-    //console.log(misDiv);
-    //console.log(misEnlaces);   
-};
-window.addEventListener("load", crearHeader, false);
+    if (window.screen.width > 1300) {
+        console.log("window.scren.width > 1300 -> MAYOR 1300");
+        header.innerHTML = "MENU de ordenador";       
 
+        header.append(nav);
+        
+    } else if (window.screen.width < 1300) {
+        console.log("window.scren.width < 1300 -> MENOR 1300");
+        header.innerHTML = "MENU de movil";
+        nav.style.position = "relative";
+        nav.style.maxHeight = "600px";                  
+        header.append(nav);         
+        document.getElementById("btn_opciones").addEventListener("click", menuHambur, false);
+    }
+
+};
+
+const menuHambur = () => {     
+    let enlaces = document.getElementsByClassName("enlace");
+    for(let e = 1; e < enlaces.length; e++){
+        if(enlaces[e].style.display == "none"){            
+            enlaces[e].style.display = "";
+        }else{            
+            enlaces[e].style.display = "none";
+        }        
+    }
+}
+window.addEventListener("load", crearHeader, false);
